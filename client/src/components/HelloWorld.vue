3d185d482b8cac7f8ed009fffa73f2a398095c41
Artur Fortunato <artur.fortunato1@gmail.com>
2018-12-18 14:09:59 +0000

    Deck of cards + board

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,99 +1,38 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div ref="board" class="red top full">
+    <!--<h1>GRINGO!</h1> -->
   </div>
 </template>
 
 <script>
+import Deck from 'deck-of-cards'
+import 'deck-of-cards/example/example.css'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  mounted: function() {
+    var deck = Deck()
+    deck.mount(this.$refs.board)
+
+    deck.cards.forEach(function (card, i) {
+      card.setSide(Math.random() < 0.5 ? 'front' : 'back');
+      card.animateTo({
+        delay: 1000 + i * 2, // wait 1 second + i * 2 ms
+        duration: 500,
+        ease: 'quartOut',
+
+        x: Math.random() * window.innerWidth - window.innerWidth / 2,
+        y: Math.random() * window.innerHeight - window.innerHeight / 2
+      });
+    });
   }
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
@@ -110,4 +49,16 @@ li {
 a {
   color: #42b983;
 }
+
+.red
+{
+  border: 1px solid red;
+}
+
+.full
+{
+  width: 100%;
+  height: 100vh;
+}
+
 </style>
