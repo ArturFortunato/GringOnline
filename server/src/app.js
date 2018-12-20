23d522af584afd4d1eb6b6e513548b46a7e54ee2
Artur Fortunato <artur.fortunato1@gmail.com>
2018-12-20 18:23:33 +0000

    Working chat

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,9 @@ const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
 const PORT = 8081
 
 app.use(morgan('combined'))
@@ -29,4 +32,17 @@ app.post('/Game', (req, res) => {
     })
 })
 
-app.listen(process.env.PORT || PORT)
+io.on('connection', function(socket) {
+    console.log("User connected")
+    socket.on('disconnect', () => {
+        console.log("User disconnected")
+    })
+    socket.on('chatmessage', function(message) {
+        io.emit('newchatmessage', message)
+    })
+
+})
+
+http.listen(process.env.PORT || PORT, function() {
+    console.log("Listening on port " + (process.env.PORT || PORT))
+})
